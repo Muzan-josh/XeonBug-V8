0b0d1bf6f17986f051e618d5149e153b8cdde46d
Muzan-josh <muzan7426@gmail.com>
2024-09-17 01:30:47 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "242066688323"
+global.ownername = "✞𖣘𝙒𝙞𝙡𝙛𝙛𝙞𝙙.𝘿𝙂95☠︎ 𖣘"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '242066688323'  //creator number
+global.ownername = '✞𖣘𝙒𝙞𝙡𝙛𝙛𝙞𝙙.𝘿𝙂95☠︎ 𖣘' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
